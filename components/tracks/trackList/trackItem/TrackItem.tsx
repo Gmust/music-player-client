ef214886fc25a66846d59493ab02e152c6768bf4
Gmust <4orgood@gmail.com>
2023-01-07 23:50:25 +0100

    Some adaptive design(not ready) in TrackItem.module.css, TrackList.module.css and added useWindowDimensions custom hook

--- components/tracks/trackList/trackItem/TrackItem.tsx
+++ components/tracks/trackList/trackItem/TrackItem.tsx
@@ -11,10 +11,15 @@ export const TrackItem: FC<ITrack> = ({ _id, comments, audio, listens, artist, p
       <div className={styles.trackInfo}>
         <h2>{name}</h2>
         <span>by: {artist}</span>
-        <Image className={styles.trackImg} src={picture} alt={''} width={200} height={200}/>
+
+        <div className={styles.trackImg} style={{}}>
+          <Image  src={picture} alt={''} width={150}  height={150}/>
+        </div>
       </div>
 
-      <ReactAudioPlayer src={audio} controls volume={0.1} />
+      <div className={styles.playerStyle}>
+        <ReactAudioPlayer src={audio} controls volume={0.1}  />
+      </div>
 
     </div>
   );
